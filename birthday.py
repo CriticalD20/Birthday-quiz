2627abdd4b948958ed557bd44878cb558da3f386
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-09-23 13:31:03 -0400

    Updated from Brython Server: 9/23/2015 1:31:03 PM

--- birthday.py
+++ birthday.py
@@ -86,33 +86,35 @@ elif birthmonth=="October":
             else:
                 print(""+name+", you are a summer baby of the two thousands.")
     elif birthyear<2000:
-        if birthmonth=="September" or birthmonth=="October" or birthmonth=="November":
-            print(""+name+", you are a fall baby of the stone age.")
-        elif birthmonth=="December" or birthmonth=="January" or birthmonth=="February":
-            print(""+name+", you are a winter baby of the stone age.")
-        elif birthmonth=="March" or birthmonth=="April" or birthmonth=="May":
-            print(""+name+", you are a spring baby of the stone age.")
+        birthday=float(input("And the day? "))
+        if birthday==int(31):
+            print("You were born on Halloween!")
+           
         else:
-            print(""+name+", you are a summer baby of the stone age.")
+            if birthmonth=="September" or birthmonth=="October" or birthmonth=="November":
+                print(""+name+", you are a fall baby of the stone age.")
+            elif birthmonth=="December" or birthmonth=="January" or birthmonth=="February":
+                print(""+name+", you are a winter baby of the stone age.")
+            elif birthmonth=="March" or birthmonth=="April" or birthmonth=="May":
+                print(""+name+", you are a spring baby of the stone age.")
+            else:
+                print(""+name+", you are a summer baby of the stone age.")
 else:
     birthyear=input("And what year were you born in, "+name+"? ")
-    if birthyear>=2000:
+    if birthyear>=str(2000):
         birthday=input("And the day? ")
         if birthmonth=="September" or birthmonth=="October" or birthmonth=="November":
-            birthday=input("And the day? ")
             print(""+name+", you are a fall baby of the two thousands.")
             
         elif birthmonth=="December" or birthmonth=="January" or birthmonth=="February":
-            birthday=input("And the day? ")
             print(""+name+", you are a winter baby of the two thousands.")
         elif birthmonth=="March" or birthmonth=="April" or birthmonth=="May":
-            birthday=input("And the day? ")
             print(""+name+", you are a spring baby of the two thousands.")
         else:
-            birthday=input("And the day? ")
             print(""+name+", you are a summer baby of the two thousands.")
             
     else:
+        birthday=input("And the day? ")
         if birthmonth=="September" or birthmonth=="October" or birthmonth=="November":
             print(""+name+", you are a fall baby of the stone age.")
         elif birthmonth=="December" or birthmonth=="January" or birthmonth=="February":
